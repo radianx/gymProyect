d42d5601628d0146c2d731361d7cae57da1cc558
Family <Family@DESKTOP-M3RF2VA.lan>
2018-12-01 22:40:24 -0300

    webcam agregado

--- src/gimnasio/vista/asd.java
+++ src/gimnasio/vista/asd.java
@@ -8,6 +8,12 @@ package gimnasio.vista;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.WebcamResolution;
+import com.github.sarxos.webcam.WebcamUtils;
+import com.github.sarxos.webcam.util.ImageUtils;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -23,18 +29,42 @@ public class asd extends javax.swing.JFrame {
         
     public asd() {
         initComponents();
+        
+        
         Webcam webcam = Webcam.getDefault();
         webcam.setViewSize(WebcamResolution.VGA.getSize());
-
+        webcam.open();
+        
+        BufferedImage image = webcam.getImage();
+        int type = image.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : image.getType();
+        
+        System.out.println(image.getWidth() + " x " + image.getHeight());
+        
+        BufferedImage resizeImage = resizeImage(image, type, 180,180);
+        
+        System.out.println(resizeImage.getWidth() + " x " + resizeImage.getHeight());
+        
         WebcamPanel panel = new WebcamPanel(webcam);
         panel.setFPSDisplayed(true);
         panel.setDisplayDebugInfo(true);
         panel.setImageSizeDisplayed(true);
         panel.setMirrored(true);
         
+        WebcamUtils.capture(webcam, "TEST1ADRIAN", ImageUtils.FORMAT_PNG);
+        
         this.add(panel);
+        this.pack();
     }
 
+    public BufferedImage resizeImage(BufferedImage original, int type, int width, int height){
+        BufferedImage resizedImage = new BufferedImage(width, height, type);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(original, 0, 0, width, height, null);
+        g.dispose();
+        return resizedImage;
+    }
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
