03ebebbb224d6b2d99bbe6e88d9de2ab95bb287a
wolix <wolix@wolix-Inspiron-7559>
2018-12-03 20:55:32 -0300

    Merge origin/master
    Conflicts:
    	src/main/java/gimnasio/modelo/AsistenciaSocio.java

---
+++ src/gimnasio/herramientas/excepciones/Notificaciones.java
@@ -0,0 +1,19 @@
+package herramientas.excepciones;
+
+/**
+ * Esta clase se utiliza para lanzar todas las excepciones que se envían a la
+ * vista desde el controlador.
+ * @author goto
+ *
+ */
+public class Notificaciones extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4984406577383662317L;
+
+	public Notificaciones(String mensaje) {
+		super(mensaje);
+	}
+}
---
+++ src/gimnasio/herramientas/excepciones/PersonaException.java
@@ -0,0 +1,13 @@
+package herramientas.excepciones;
+
+public class PersonaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6738959412521053914L;
+
+	public PersonaException(String mensaje) {
+		super(mensaje);
+	}
+}
